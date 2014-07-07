8b143c4c0d469a4ba46f5abc6d20d1bf1585ab51
Lorna Marlow <bikergirl1515@hotmail.com>
2014-07-07 16:03:13 +0000

    added s3 details

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,11 +5,7 @@ class Project < ActiveRecord::Base
     square: '200x200#',
     medium: '300x300>'
   }
-  
-    :url => ':s3_alias_url',
-    :s3_host_alias => 'mystitching.s3.amazonaws.com', 
-    :bucket => 'mystitching',
-    :path => "images/:class/:id.:style.:extension"
+
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :finished_image, :content_type => /\Aimage\/.*\Z/
 end
