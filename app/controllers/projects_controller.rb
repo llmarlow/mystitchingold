33bf2e231444e6ac7169b9935e14a61b65a59646
Lorna Marlow <bikergirl1515@hotmail.com>
2014-07-14 10:41:34 +0000

    Home page shows current user projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @projects = Project.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    @projects = @user.projects.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
   end
 
   def show
